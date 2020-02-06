4df5cd2672a1309a29f41bd0325f12531aa5b5d1
Andrei Navas <35807760+andreinavas@users.noreply.github.com>
2020-02-05 23:20:34 -0500

    Update application.py

--- application.py
+++ application.py
@@ -10,9 +10,6 @@ app = Flask(__name__)
 def hello():
     return "Hello! I am the container: {}\n".format(socket.gethostname())
 
-@app.route("/user")
-def user():
-    return "Hello User! You are in the container: {}\n".format(socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5050)
